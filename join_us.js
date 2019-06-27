98ac82f7104c2b229b7b86bce6d339500991f945
Johanne <johanne_lewis@hotmail.com>
2019-06-27 13:40:34 +0100

    Establish a connection with mysql db

--- join_us.js
+++ join_us.js
@@ -1,2 +1,9 @@
 var faker = require('faker');
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password123',
+  database : 'join_us'
+});
